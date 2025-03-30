585dd6e0b03328b62106ee5af677cd53b6da3d80
Marzanur Zarif <93573851+MarzanurZarif@users.noreply.github.com>
2025-03-30 19:21:51 +0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,15 +7,18 @@ end
 print("Game Loaded...")
 
 queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+local autoexe = coroutine.create(function()
+	local TeleportCheck = false
+	print("Initiating autoexe...")
+	game.Players.LocalPlayer.OnTeleport:Connect(function(State)
+		if (not TeleportCheck) and queueteleport then
+			TeleportCheck = true
+			queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/MarzanurZarif/PS99/refs/heads/main/loader.lua'))()")
+		end
+	end)
+end)
+coroutine.resume(autoexe)
 
 print("Loading Script...")
 task.wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Verteniasty/Pet-rbx/refs/heads/main/loadstring"))()
-
-local TeleportCheck = false
-game.Players.LocalPlayer.OnTeleport:Connect(function(State)
-	if (not TeleportCheck) and queueteleport then
-		TeleportCheck = true
-		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/MarzanurZarif/PS99/refs/heads/main/loader.lua'))()")
-	end
-end)
